7af714531d968c94b35c5f33f14e5ca91a3a501a
Lara <76517940+lara-dev92@users.noreply.github.com>
2021-11-19 22:59:13 +0200

    copyright added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,17 +10,22 @@ function Footer(){
 
 <div className="footer">
 
-<p>Connect with us</p>
-<div className="social">
-<a href="https://www.youtube.com/"
-  className="youtube">
-  <FontAwesomeIcon icon={faYoutube} size="2x" />
-</a>
-<a href="https://www.facebook.com/"
-  className="facebook">
-  <FontAwesomeIcon icon={faFacebook} size="2x" />
-</a>
-</div>
+      <p>Connect with us</p>
+      <div className="social">
+      <a href="https://www.youtube.com/"
+        className="youtube">
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a href="https://www.facebook.com/"
+        className="facebook">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      </div>
+
+
+<p className="copyright">
+(c)Copyright 2021  
+</p>
 
 </div>
 )
