c2623a0968a3dc1696a1645ff8253c3fa8aa1a8b
Lara <76517940+lara-dev92@users.noreply.github.com>
2021-11-20 16:16:25 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
+import Home from './components/Home.js'
+import Pricing from './components/Pricing.js'
+import Solutions from './components/Solutions.js'
+import Contact from './components/Contact.js'
 
 
+{/*
 function App() {
   return (
     <div className="App">
@@ -13,3 +26,51 @@ function App() {
 }
 
 export default App;
+
+*/}
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/solutions">Solutions</Link>
+            </li>
+            <li>
+              <Link to="/pricing">Pricing</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/solutions">
+            <Solutions />
+          </Route>
+          <Route path="/pricing">
+            <Pricing />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+
+
